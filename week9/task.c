913d7880fa84aa6acf6608307cbf1f050d08f258
Rostislav Tsachev <rosti_99@abv.bg>
2020-12-01 13:36:43 +0200

    Add week9

---
+++ week9/task.c
@@ -0,0 +1,45 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef int bool;
+#define true 1;
+#define false 0;
+
+const int UPPER_BOUND = 10E4;
+
+int main(int argc, char* argv[]) {
+    int fd = open("f", O_CREAT | O_TRUNC | O_RDWR, 0644);
+    int a = 0;
+    char buff[64];
+
+    write(fd, &a, sizeof(int));
+
+    int pid = fork();
+    
+    for (int i = 0; i < UPPER_BOUND; i++) {
+        lseek(fd, 0, 0);
+        read(fd, &a, sizeof(int));
+        if (pid == 0)
+            a += 1;
+        else
+            a += 2;
+        if (pid == 0)
+            sprintf(buff, "     %d\n", a);
+        else 
+            sprintf(buff, "%d\n", a);
+        
+            
+        write(1, buff, strlen(buff));
+
+        lseek(fd, 0, 0);
+        write(fd, &a, sizeof(int));
+    }
+
+
+	return 0;
+}
